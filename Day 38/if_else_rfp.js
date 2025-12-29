1f93ac9aa64c7e2e39353810190b098661baa014
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2025-12-29 16:57:48 +0530

    First commit

---
+++ Day 38/if_else_rfp.js
@@ -0,0 +1,19 @@
+ var num1  = Math.floor(Math.random() * 900) + 100;
+    var num2  = Math.floor(Math.random() * 900) + 100;
+    var num3  = Math.floor(Math.random() * 900) + 100;
+
+    console.log("The minimium of 3 numbers is: " +Math.min(num1,num2,num3));
+    console.log("The minimium of 3 numbers is: " + Math.max(num1,num2,num3));
+
+
+
+const toss = Math.random() < 0.5 ? 0 : 1;
+if(toss==0){
+    console.log("Heads")
+} else{
+    console.log("Tails")
+}
+
+
+
+
