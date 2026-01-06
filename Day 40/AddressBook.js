df8a8850959bc97aba42b4e1fe39b737718c0a26
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-01-06 14:48:36 +0530

    UC 2 - Ability to ensure Valid

--- Day 40/AddressBook.js
+++ Day 40/AddressBook.js
@@ -62,6 +62,7 @@ class Contact{
         const pattern = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
         if (!pattern.test(email)) throw new Error("Invalid Email");
     }
+    
 
 }
 
